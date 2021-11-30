a5a37299fc1ff82186c6da99ef26895f8c955460
zubetcha <zuhye5@gmail.com>
2021-11-30 22:52:52 +0900

    add upload button at main page

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -3,11 +3,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = (props) => {
-    const { text, _onClick } = props;
+    const { text, _onClick, is_float } = props;
 
+    if (is_float) {
+        return (
+            <React.Fragment>
+                <FloatButton onClick={_onClick}>{text}</FloatButton>
+            </React.Fragment>
+        )    
+    }
+    
     return (
         <React.Fragment>
-            <Btn onClick={_onClick}>{text}</Btn>
+            <Btn onClick={_onClick} is_float={is_float}>{text}</Btn>
         </React.Fragment>
     )
 }
@@ -15,6 +23,7 @@ const Button = (props) => {
 Button.defaultProps = {
     text: "텍스트",
     _onClick: () => {},
+    is_float: false,
 }
 
 const Btn = styled.button`
@@ -25,6 +34,23 @@ const Btn = styled.button`
     outline: none;
     border: 0;
     box-sizing: border-box;
+`;
+
+const FloatButton = styled.button`
+    width: 50px;
+    height: 50px;
+    background-color: tomato;
+    color: whitesmoke;
+    box-sizing: border-box;
+    font-size: 36px;
+    font-weight: 800;
+    position: fixed;
+    bottom: 50px;
+    right: 16px;
+    text-align: center;
+    vertical-align: middle;
+    border: none;
+    border-radius: 50%;
 `;
 
 export default Button;
--- src/shared/App.js
+++ src/shared/App.js
@@ -7,7 +7,7 @@ import PostList from "../pages/PostList";
 import Login from "../pages/Login";
 import Join from "../pages/Join";
 import Header from "../components/Header";
-import { Grid } from "../elements";
+import { Button, Grid } from "../elements";
 import Permit from "./Permit";
 
 import { useDispatch } from "react-redux";
@@ -16,18 +16,16 @@ import { actionCreators as userActions } from "../redux/modules/user";
 import { apiKey } from "./firebase";
 
 function App() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const _session_key=`firebase:authUser:${apiKey}:[DEFAULT]`;
-    const is_session = sessionStorage.getItem(_session_key) ? true : false;
+  const _session_key = `firebase:authUser:${apiKey}:[DEFAULT]`;
+  const is_session = sessionStorage.getItem(_session_key) ? true : false;
 
-    React.useEffect(() => {
-        if(is_session) {
-            dispatch(userActions.loginCheckFB());
-        }
-    })
-
-    
+  React.useEffect(() => {
+    if (is_session) {
+      dispatch(userActions.loginCheckFB());
+    }
+  });
 
   return (
     <div className="App">
@@ -40,10 +38,8 @@ function App() {
         </ConnectedRouter>
       </Grid>
       <Permit>
-          <div style={{backgroundColor: "tomato", width: "50px", height: "50px"}}>
-              작성하기
-          </div>
-      </Permit>
+          <Button is_float text="+"></Button>
+        </Permit>
     </div>
   );
 }
